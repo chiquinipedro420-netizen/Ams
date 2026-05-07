71f8c3c227236655be434aa8ac6b636b434da1c3
chiquinipedro420-netizen <chiquinipedro420@gmail.com>
2026-05-07 15:32:16 -0300

    Add files via upload

--- pw1/Nova pasta/JS/scrip.js
+++ pw1/Nova pasta/JS/scrip.js
@@ -2,14 +2,26 @@ const botao = document.getElementById("tema");
 
 //carregar preferência
 if (localStorage.getItem("tema") === "dark"){
-    document.body.classList.add("dark");
+    document.body.classList.add("dark")
 }
 
 //alternar tema
 botao.addEventListener("click",() => {
     document.body.classList.toggle("dark");
 
-if (document.body.classList.contains("darki")){}
+if (document.body.classList.contains("dark")){}
+
+const botao= document.getElementById("tema");
+
+constupdateBUttomIcon = () => {
+    if (button) return;
+const darkmode = document.body.classList.contains("dark");
+botao.textContent = darkmode? "☀️" : "🌕";
+botao.title = darkmode? "mudar para modo escuro":"mudar para modo claro" ;
+}:
+
+const 
+
 }
 
 )
