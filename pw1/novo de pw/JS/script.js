e3cfc61cad15fa4f6fab7d7d5d45a15b5510dacd
chiquinipedro420-netizen <chiquinipedro420@gmail.com>
2026-05-21 13:32:41 -0300

    Add files via upload

---
+++ pw1/novo de pw/JS/script.js
@@ -0,0 +1,43 @@
+/*-const botao = document.getElementById("Tema");
+
+if (localStorage.getItem("Tema") === "dark"){
+    document.body.classList.add("dark");
+}
+
+botao.addEventListener("click", () => { 
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("Tema", "dark");
+    } else {
+        localStorage.setItem("Tema", "light");
+    }
+});*/
+
+const botao= document.getElementById("tema");
+
+const updateButtonIcon = () => {
+    if (!botao) return;
+    const darkMode = document.body.classList.contains("dark")
+    botao.textContent = darkMode ? "☀️" : "🌙";
+    botao.title = darkMode ? "Mudar para modo claro" : "Mudar para modo escuro";
+};
+
+const TemaAtual = localStorage.getItem("tema") === "dark" ? "dark" : "light";
+if(TemaAtual === "dark") {
+    document.body.classList.add("dark");
+}
+
+updateButtonIcon();
+
+if (botao) {
+    botao.addEventListener("click", () => { document.body.classList.toggle("dark");
+
+        if(document.body.classList.contains("dark")) {
+            localStorage.setItem("tema", "dark");
+        } else {
+            localStorage.setItem("tema", "light");
+        }
+
+        updateButtonIcon ();
+    });
+}
